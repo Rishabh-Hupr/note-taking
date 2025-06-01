75a92ec8b2d5f85cf536eade8cfdc21c64702424
Rishabh-Hupr <dewangpant@gmail.com>
2025-06-01 17:54:01 +0100

    1June: Firing up go re-write 😎, lets go baby 🚀

---
+++ go-rewrite/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/ncruces/zenity"
+)
+
+func errorCheck(err error) bool {
+	if err != nil {
+		println("Something wrong happened")
+		return false
+	}
+	return true
+}
+
+func main() {
+	// var key string
+	// var value string
+	input, err := zenity.Entry("Enter the key", zenity.Title("Storing note..."))
+	if errorCheck(err) {
+		println("Key", input, "was input")
+	}
+
+	value, err := zenity.Entry("Enter the value", zenity.Title("Storing note..."))
+	if errorCheck(err) {
+		println("Entered value", value)
+	}
+
+	// key_reader := bufio.NewReader(os.Stdin)
+	// print("Enter key: ")
+	// key, err := key_reader.ReadString('\n')
+	// if err != nil {
+	// 	println("Something wrong happend 🚨")
+	// 	return
+	// }
+
+	// value_reader := bufio.NewReader(os.Stdin)
+	// print("Enter value: ")
+	// value, err := value_reader.ReadString('\n')
+	// if err != nil {
+	// 	println("Something wrong happened 🚨")
+	// 	return
+	// }
+	// store := map[string]string{}
+	// store[key] = value
+
+	// val, ok := store[key]
+
+	// if ok {
+	// 	print("Key found", val)
+	// } else {
+	// 	print("Hey hey hey, it's not something you ever stored")
+	// }
+}
